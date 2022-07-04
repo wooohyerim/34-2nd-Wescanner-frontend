5bdb52b32a7a4dbc95aa8908262d12e1ca84f450
woohyerim <hyerim3414@gmail.com>
2022-07-04 16:34:20 +0900

    Add: 초기세팅 완료

---
+++ src/Router.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
+import Main from './pages/Main/Main';
+import Login from './pages/Login/Login';
+import List from './pages/List/List';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/list" element={<List />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default Router;
