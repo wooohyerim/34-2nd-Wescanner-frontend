d2f9d36eb8d2736b2187bb1b0f79b6b0859f5045
Bo-Mi Kim <bomi@Bo-Mis-MacBook-Pro.local>
2022-07-07 11:03:54 +0900

    MODIFY: 리팩토링 완료

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer/Footer';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
 import List from './pages/List/List';
+import LoginLoading from './pages/Login/LoginLoading';
 
 const Router = () => {
   return (
@@ -14,6 +15,7 @@ const Router = () => {
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/list" element={<List />} />
+        <Route path="/loginloading" element={<LoginLoading />} />
       </Routes>
       <Footer />
     </BrowserRouter>
